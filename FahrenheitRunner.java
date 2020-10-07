41333d0e39bf145df40aabd23a2ae042a8693e21
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-07 11:36:01 -0500

    Add files via upload

---
+++ FahrenheitRunner.java
@@ -0,0 +1,16 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class FahrenheitRunner
+{
+	public static void main( String[] args )
+	{
+		//add test cases
+				
+	}
+}
